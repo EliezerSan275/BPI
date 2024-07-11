fd9996bdc679aad0826a019aff91190f6cc0a1f6
AGV08 <alebrije_galva@hotmail.com>
2024-07-11 12:12:32 -0600

    Seccion busqueda y cambios menores
    Se agrego el input de busqueda y se puso un alert de mientras para simular que busca algo, al igual este tiene un observer el cual llama al alert despues de 1 segundo de que se dejo de escribir, al igual que se cambio algunos estilos para mejorar lo responsive

--- src/app/main-header/main-header.component.ts
+++ src/app/main-header/main-header.component.ts
@@ -1,18 +1,56 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-main-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    RouterLink
+  ],
   templateUrl: './main-header.component.html',
-  styleUrl: './main-header.component.css'
+  styleUrls: ['./main-header.component.css']
 })
 export class MainHeaderComponent {
+  private inputSubject = new Subject<string>();
+  private debounceTimeInMs = 1000; // Tiempo de espera después de que el usuario deja de escribir (en milisegundos)
+  isSearchVisible = false; // Controla la visibilidad del campo de búsqueda
+
+  constructor(private router: Router) {
+    // Configura el observador para manejar los cambios de input con debounceTime
+    this.inputSubject.pipe(
+      debounceTime(this.debounceTimeInMs)
+    ).subscribe(value => {
+      this.showAlert(value);
+    });
+  }
 
   @Output() toggleDropdown = new EventEmitter<void>();
 
+  navigateTo(route: string) {
+    this.router.navigate([route]);
+  }
+
   onToggleMenu() {
     this.toggleDropdown.emit();
+  }
+
+  onInputChange(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.inputSubject.next(input.value);
+  }
+
+  showAlert(value: string) {
+    if(value != ""){
+      alert(`Has escrito: ${value}`);
+    }
+  }
+
+  toggleSearchVisibility() {
+    this.isSearchVisible = !this.isSearchVisible;
   }
 }
