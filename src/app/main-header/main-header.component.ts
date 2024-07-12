3f6687a0442dfe03e89465d2cc1a42319761c5bd
AGV08 <alebrije_galva@hotmail.com>
2024-07-12 13:37:49 -0600

    Quitar boton busqueda del header
    se quito el boton de busqueda del header por que despues se añadira en otra parte

--- src/app/main-header/main-header.component.ts
+++ src/app/main-header/main-header.component.ts
@@ -1,8 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router, RouterLink, RouterOutlet } from '@angular/router';
-import { Subject } from 'rxjs';
-import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-main-header',
@@ -16,18 +14,9 @@ import { debounceTime } from 'rxjs/operators';
   styleUrls: ['./main-header.component.css']
 })
 export class MainHeaderComponent {
-  private inputSubject = new Subject<string>();
-  private debounceTimeInMs = 1000; // Tiempo de espera después de que el usuario deja de escribir (en milisegundos)
   isSearchVisible = false; // Controla la visibilidad del campo de búsqueda
 
-  constructor(private router: Router) {
-    // Configura el observador para manejar los cambios de input con debounceTime
-    this.inputSubject.pipe(
-      debounceTime(this.debounceTimeInMs)
-    ).subscribe(value => {
-      this.showAlert(value);
-    });
-  }
+  constructor(private router: Router) {}
 
   @Output() toggleDropdown = new EventEmitter<void>();
 
@@ -39,18 +28,4 @@ export class MainHeaderComponent {
     this.toggleDropdown.emit();
   }
 
-  onInputChange(event: Event) {
-    const input = event.target as HTMLInputElement;
-    this.inputSubject.next(input.value);
-  }
-
-  showAlert(value: string) {
-    if(value != ""){
-      alert(`Has escrito: ${value}`);
-    }
-  }
-
-  toggleSearchVisibility() {
-    this.isSearchVisible = !this.isSearchVisible;
-  }
 }
